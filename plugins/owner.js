93046107b4c5580dd8711ebf4aca82d6e62abf0e
XX88IP <xx88ip@gmail.com>
2026-05-12 22:31:20 +0100

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,16 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
   let caption = `
-*「 معلومات عن صاحب البوت 」*\n\n
-*Whatsapp channel:*\n https://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A\n
-*instagram:*\ninstagram.com/noureddine_ouafy
-
-*youtube:*\nyoutube.com/@noureddineouafy2
-
-*facebook page:*\nwww.facebook.com/profile.php?id=100063533185520
-
-*script bot :* github.com/noureddineouafy
-
+_BOT OWNER INFO_
+https://wa.me/qr/FFA4RBEQZQG6O1
 `.trim()
   m.reply(caption)
 }
