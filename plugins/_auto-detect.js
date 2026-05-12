c940cefd2da37b764b30452e38e9c9b3a8a53447
XX88IP <xx88ip@gmail.com>
2026-05-12 23:14:24 +0100

    Update _auto-detect.js

--- plugins/_auto-detect.js
+++ plugins/_auto-detect.js
@@ -2,129 +2,155 @@ import { WAMessageStubType } from "@adiwajshing/baileys";
 
 export async function before(m) {
   let chat = db.data.chats[m.chat];
+
   if (chat.detect) {
+
     if (!m.messageStubType || !m.isGroup) return;
+
     let edtr = `@${m.sender.split`@`[0]}`;
+
     if (m.messageStubType == 21) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} change the Group Subject to :\n*${m.messageStubParameters[0]}*`,
+          text: `${edtr} changed the group subject to:\n*${m.messageStubParameters[0]}*`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 22) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} has changed the group icon.`,
+          text: `${edtr} changed the group icon.`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (
       m.messageStubType == 1 ||
       m.messageStubType == 23 ||
       m.messageStubType == 132
     ) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} *reset* group link!\n\n`,
+          text: `${edtr} reset the group link.`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 24) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} change the group description.\n\n${m.messageStubParameters[0]}`,
+          text: `${edtr} changed the group description.\n\n${m.messageStubParameters[0]}`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 25) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} have arranged so *${m.messageStubParameters[0] == "on" ? "admin only" : "all participants"}* which can edit group info.`,
+          text: `${edtr} changed group info settings to *${m.messageStubParameters[0] == "on" ? "admin only" : "all participants"}*.`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 26) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} telah *${m.messageStubParameters[0] == "on" ? "close" : "open"}* group!\nNow ${m.messageStubParameters[0] == "on" ? "admin only Which" : "all participants"} can send messages.`,
+          text: `${edtr} *${m.messageStubParameters[0] == "on" ? "closed" : "opened"}* the group.\nNow ${m.messageStubParameters[0] == "on" ? "only admins" : "all participants"} can send messages.`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 29) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} has made @${m.messageStubParameters[0].split`@`[0]} as an admin.`,
+          text: `${edtr} promoted @${m.messageStubParameters[0].split`@`[0]} to admin.`,
           mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 30) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} has terminated @${m.messageStubParameters[0].split`@`[0]} from admin.`,
+          text: `${edtr} demoted @${m.messageStubParameters[0].split`@`[0]} from admin.`,
           mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 72) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} تغيير مدة الرسائل المؤقتة إلى *@${m.messageStubParameters[0]}*`,
+          text: `${edtr} changed disappearing messages duration to *${m.messageStubParameters[0]}*`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else if (m.messageStubType == 123) {
+
       await this.sendMessage(
         m.chat,
         {
-          text: `${edtr} *تعطيل الرسائل* مؤقتًا.`,
+          text: `${edtr} disabled disappearing messages.`,
           mentions: [m.sender],
         },
         {
           quoted: fkon,
         },
       );
+
     } else {
+
       console.log({
         messageStubType: m.messageStubType,
         messageStubParameters: m.messageStubParameters,
         type: WAMessageStubType[m.messageStubType],
       });
+
     }
   }
 }
